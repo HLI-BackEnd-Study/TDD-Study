b8ca415b8605e76d98a06524175191fa3ba10a1b
강돈희(2230194) <herbtea@hanwha.com>
2024-06-07 15:45:41 +0900

    feat : Ball테스트 코드 작성

--- herbtea30/baseball/src/test/java/com/hanwha/domain/BallTest.java
+++ herbtea30/baseball/src/test/java/com/hanwha/domain/BallTest.java
@@ -1,8 +1,40 @@
 package com.hanwha.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 @DisplayName("볼 테스트")
-public class BallTest {
+class BallTest {
+    Ball ball;
+    @BeforeEach
+    public void setUp() {
+        ball = new Ball(123);
+    }
 
+    @Test
+    @DisplayName("Ball은 3자리 수를 가짐")
+    void ballTest1() {
+        assertThat(ball.getTotalBall()).isLessThan(1000);
+        assertThat(ball.getTotalBall()).isGreaterThan(99);
+
+        assertThatThrownBy(() -> {
+            ball = new Ball(1234);
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage("볼은 3자리 숫자만 입력할 수 없습니다.");
+
+        assertThatThrownBy(() -> {
+            ball = new Ball(12);
+        }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("볼은 3자리 숫자만 입력할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("Ball은 각 자리수의 범위 체크(1 ~ 9)")
+    void ballTest2() {
+        assertThat(ball.getFirstBall()).isEqualTo(1);
+        assertThat(ball.getSecondBall()).isEqualTo(2);
+        assertThat(ball.getThirdBall()).isEqualTo(3);
+    }
 }
