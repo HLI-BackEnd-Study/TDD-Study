4875d7efedb740120da5079a14f980270e0d6a36
김민지(2210188) <mjkim103301@gmail.com>
2024-06-11 08:21:15 +0900

    refactor: const file에 private 생성자 추가

--- mjkim103301/baseball/src/main/java/com/hanwha/view/Message.java
+++ mjkim103301/baseball/src/main/java/com/hanwha/view/Message.java
@@ -1,6 +1,7 @@
 package com.hanwha.view;
 
 public class Message {
+    private Message(){}
     static final String USER_INPUT = "1~9 범위의 숫자 3자리를 입력해 주세요:";
     static final String SUCCESS = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     static final String RETRY = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
