8c982097dff7fbad2fadfe63a9675a1ecb0a24e5
mjkim <mjkim103301@gmail.com>
2024-06-11 01:08:05 +0900

    feat: 게임 동작 기능, 출력 메시지 추가

--- mjkim103301/baseball/src/main/java/com/hanwha/view/GamePlay.java
+++ mjkim103301/baseball/src/main/java/com/hanwha/view/GamePlay.java
@@ -1,5 +1,60 @@
 package com.hanwha.view;
 
+import com.hanwha.player.Computer;
+import com.hanwha.player.User;
+import com.hanwha.service.Solution;
+
+import java.util.Scanner;
+
 public class GamePlay {
+    static Computer computer;
+    static User user;
+    static Solution solution;
+    static boolean isSuccess;
+    static Scanner scanner;
+
+    public void init() {
+        isSuccess = false;
+        computer = new Computer();
+        solution = new Solution();
+        scanner = new Scanner(System.in);
+    }
+
+    public void play() {
+        init();
+        String answer = String.valueOf(computer.getNumber());
+        while (!isSuccess) {
+            System.out.println(Message.USER_INPUT);
+            int input = scanner.nextInt();
+            user = new User(input);
+            if (solution.isSuccess(answer, String.valueOf(user.number()))) {
+                isSuccess = true;
+                finish();
+                break;
+            }
+            printScore(answer, String.valueOf(user.number()));
+        }
+    }
+
+    public void printScore(String answer, String input) {
+        int strike = solution.getStrikeCount(answer, input);
+        int ball = solution.getBallCount(answer, input);
+        boolean nothing = solution.isNothing(strike, ball);
+        if (nothing) {
+            System.out.println(Message.NOTHING);
+            return;
+        }
+        System.out.println(strike + Message.STRIKE + ", " + ball + Message.BALL);
+    }
+
+    public void finish() {
+        System.out.println(Message.SUCCESS);
+        System.out.println(Message.RETRY);
+        int retry = scanner.nextInt();
+        if (retry == 2) {
+            return;
+        }
+        play();
+    }
 
 }
---
+++ mjkim103301/baseball/src/main/java/com/hanwha/view/Message.java
@@ -0,0 +1,10 @@
+package com.hanwha.view;
+
+public class Message {
+    static final String USER_INPUT = "1~9 범위의 숫자 3자리를 입력해 주세요:";
+    static final String SUCCESS = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    static final String RETRY = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    static final String BALL = "볼";
+    static final String STRIKE = "스트라이크";
+    static final String NOTHING = "낫싱";
+}
