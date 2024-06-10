0542a8d9660553813f90eaa8b63f0a975897c1b7
ch200203 <ch200205@naver.com>
2024-06-10 13:25:55 +0900

    refactor : 메시지 상수화

---
+++ ch200203/baseball/src/main/java/com/hanwha/constant/MessageConstant.java
@@ -0,0 +1,6 @@
+package com.hanwha.constant;
+
+public class MessageConstant {
+
+    public static String NUMBER_BOUND_EXCEPTION = "숫자는 1~9까지 입니다.";
+}
--- ch200203/baseball/src/main/java/com/hanwha/domain/BaseBallNumber.java
+++ ch200203/baseball/src/main/java/com/hanwha/domain/BaseBallNumber.java
@@ -2,6 +2,7 @@ package com.hanwha.domain;
 
 import static com.hanwha.constant.BaseballConstant.MAX_NUMBER;
 import static com.hanwha.constant.BaseballConstant.MIN_NUMBER;
+import static com.hanwha.constant.MessageConstant.NUMBER_BOUND_EXCEPTION;
 
 public class BaseBallNumber {
 
@@ -18,7 +19,7 @@ public class BaseBallNumber {
 
     private void validNumber(final int number) {
         if (number < MIN_NUMBER || number > MAX_NUMBER) {
-            throw new IllegalArgumentException("숫자는 1~9까지 입니다.");
+            throw new IllegalArgumentException(NUMBER_BOUND_EXCEPTION);
         }
     }
 
