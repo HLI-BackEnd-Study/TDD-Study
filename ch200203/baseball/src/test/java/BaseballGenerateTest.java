078db64fb9f83644ecf19e3e19b56c6d4daa5d54
ch200203 <ch200205@naver.com>
2024-06-10 13:37:29 +0900

    test : 생성된 난수에 중복된 숫자가 없는지 검증

--- ch200203/baseball/src/test/java/BaseballGenerateTest.java
+++ ch200203/baseball/src/test/java/BaseballGenerateTest.java
@@ -1,9 +1,11 @@
 import com.hanwha.generator.RandomBaseballNumberGenerator;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static com.hanwha.constant.BaseballConstant.GAME_COUNT;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class BaseballGenerateTest {
 
@@ -11,6 +13,14 @@ public class BaseballGenerateTest {
     @DisplayName("컴퓨터가 랜덤한 게임 수 자리만큼의 난수를 생성할 수 있다.")
     void generateTest() {
         RandomBaseballNumberGenerator randomBaseballNumberGenerator = new RandomBaseballNumberGenerator();
-        Assertions.assertThat(randomBaseballNumberGenerator.generate()).hasSize(GAME_COUNT);
+        assertThat(randomBaseballNumberGenerator.generate()).hasSize(GAME_COUNT);
+    }
+
+    @Test
+    @DisplayName("생성된 난수에 중복된 숫자가 없는지 검증한다.")
+    void generateUniqueNumbersTest() {
+        RandomBaseballNumberGenerator randomBaseballNumberGenerator = new RandomBaseballNumberGenerator();
+        List<Integer> generatedNumbers = randomBaseballNumberGenerator.generate();
+        assertThat(generatedNumbers).doesNotHaveDuplicates();
     }
 }
