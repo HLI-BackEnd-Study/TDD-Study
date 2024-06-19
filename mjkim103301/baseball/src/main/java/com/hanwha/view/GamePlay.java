33a218281a5cac37ae681e4d75c682ec4bf0ffdb
김민지(2210188) <mjkim103301@gmail.com>
2024-06-19 12:46:37 +0900

    refactor: 숫자가 아닌 문자가 들어왔을 때 예외처리 추가, finish로직 예외처리 추가

--- mjkim103301/baseball/src/main/java/com/hanwha/view/GamePlay.java
+++ mjkim103301/baseball/src/main/java/com/hanwha/view/GamePlay.java
@@ -4,6 +4,7 @@ import com.hanwha.player.Computer;
 import com.hanwha.player.User;
 import com.hanwha.service.Solution;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class GamePlay {
@@ -35,11 +36,14 @@ public class GamePlay {
         User user;
         while (true) {
             try {
-                println(Message.USER_INPUT);
+                println(MessageConstant.USER_INPUT);
                 int input = scanner.nextInt();
                 user = new User(input);
                 user.checkValidInput();
                 break;
+            } catch (InputMismatchException e) {
+                println(MessageConstant.INPUT_MISMATCH);
+                scanner = new Scanner(System.in);
             } catch (IllegalArgumentException e) {
                 println(e.getMessage());
             }
@@ -53,26 +57,43 @@ public class GamePlay {
 
     public void printScore(String answer, String input) {
         if (isSuccess) {
-            println(Message.SUCCESS);
+            println(MessageConstant.SUCCESS);
             return;
         }
         int strike = solution.getStrikeCount(answer, input);
         int ball = solution.getBallCount(answer, input);
         boolean nothing = solution.isNothing(strike, ball);
         if (nothing) {
-            println(Message.NOTHING);
+            println(MessageConstant.NOTHING);
             return;
         }
-        println(strike + Message.STRIKE + ", " + ball + Message.BALL);
+        println(strike + MessageConstant.STRIKE + ", " + ball + MessageConstant.BALL);
     }
 
     public void finish() {
-        println(Message.RETRY);
-        int retry = scanner.nextInt();
-        if (retry == 2) {
-            return;
+        while (true) {
+            try {
+                println(MessageConstant.RETRY);
+                int retry = scanner.nextInt();
+                handleRetry(retry);
+                break;
+            } catch (InputMismatchException e) {
+                println(MessageConstant.INPUT_MISMATCH);
+                scanner = new Scanner(System.in);
+            } catch (Exception e) {
+                println(e.getMessage());
+            }
         }
-        play();
+    }
+
+    private void handleRetry(int retry) {
+        if (retry == 2) {
+            System.exit(0);
+        } else if (retry == 1) {
+            play();
+        } else {
+            throw new IllegalArgumentException(String.format("%s는 알맞지 않은 명령어입니다.", retry));
+        }
     }
 
     private void println(String message) {
