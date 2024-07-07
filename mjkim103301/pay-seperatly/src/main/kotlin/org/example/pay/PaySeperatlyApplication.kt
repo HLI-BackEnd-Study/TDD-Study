968f8a05ea99f77c70e2821d1eab4fce7373f096
mjkim <mjkim103301@gmail.com>
2024-07-07 18:19:35 +0900

    chore: 보험료정산시스템 환경설정

---
+++ mjkim103301/pay-seperatly/src/main/kotlin/org/example/pay/PaySeperatlyApplication.kt
@@ -0,0 +1,11 @@
+package org.example.pay
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaySeperatlyApplication
+
+fun main(args: Array<String>) {
+	runApplication<PaySeperatlyApplication>(*args)
+}
---
+++ mjkim103301/pay-seperatly/src/test/kotlin/org/example/pay/PaySeperatlyApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.pay
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaySeperatlyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
