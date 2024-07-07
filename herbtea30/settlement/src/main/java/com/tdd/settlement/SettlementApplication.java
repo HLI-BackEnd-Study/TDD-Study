d744864705a6bcaf47257c734c3254db2ccc0842
herbtea <herbtea30@gmail.com>
2024-07-07 21:26:16 +0900

    init commit

---
+++ herbtea30/settlement/src/main/java/com/tdd/settlement/SettlementApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.settlement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SettlementApplication.class, args);
+    }
+
+}
---
+++ herbtea30/settlement/src/test/java/com/tdd/settlement/SettlementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd.settlement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettlementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
