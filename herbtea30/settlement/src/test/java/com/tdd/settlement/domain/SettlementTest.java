e7d3df19a4616cf6b2af00236af69478585bfae2
herbtea <herbtea30@gmail.com>
2024-07-08 21:38:17 +0900

    test: 정산요청하기 케이스 정의

---
+++ herbtea30/settlement/src/test/java/com/tdd/settlement/domain/SettlementTest.java
@@ -0,0 +1,50 @@
+package com.tdd.settlement.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class SettlementTest {
+
+    @DisplayName("정산 요청하기 - N빵")
+    @Test
+    public void test1() {
+        Settlement settlement = new Settlement();
+        Amount requestAmount = new Amount(100000L);
+        List<User> userList = List.of(new User("kdh"), new User("leehj"), new User("kimmj"), new User("inch"));
+        settlement.requestSettlement(requestAmount);
+    }
+
+    @DisplayName("정산 요청하기 - 지정")
+    @Test
+    public void test1() {
+        Settlement settlement = new Settlement();
+        List<User> userList = List.of(new User("kangdh", 30000L), new User("leehj", 25000L), new User("kimmj", 25000L), new User("inch", 20000L));
+        Amount requestAmount = new Amount(10000L);
+        settlement.requestSettlement(requestAmount);
+    }
+
+    @DisplayName("정산 금액 보내기")
+    @Test
+    public void test2() {
+    }
+
+    @DisplayName("정신 금액 받기")
+    @Test
+    public void test3() {
+    }
+
+    @DisplayName("정산 요청내역")
+    @Test
+    public void test4(){
+    }
+
+    @DisplayName("정산 보낸내역")
+    @Test
+    public void test5() {
+    }
+}
