f2c82e79761c41b82c041b337a3489e5594e5fa0
김민지(2210188) <mjkim103301@gmail.com>
2024-07-15 20:59:54 +0900

    chore: 프로젝트명 변경. 오타 수정
    seperatly -> separately

--- mjkim103301/pay-separately/build.gradle.kts
+++ mjkim103301/pay-separately/build.gradle.kts
@@ -1,8 +1,11 @@
 plugins {
 	id("org.springframework.boot") version "3.3.1"
 	id("io.spring.dependency-management") version "1.1.5"
+	id("io.freefair.lombok") version "8.1.0"
+	kotlin("plugin.lombok") version "2.0.0"
 	kotlin("jvm") version "1.9.24"
 	kotlin("plugin.spring") version "1.9.24"
+	kotlin("plugin.power-assert") version "2.0.0"
 }
 
 group = "org.example"
@@ -20,7 +23,10 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	runtimeOnly ("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
